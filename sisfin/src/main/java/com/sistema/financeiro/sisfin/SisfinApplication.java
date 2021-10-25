f27a3844736df032ebce74e795aa07e66a6bbb84
Tales Magalhaes <talesribeiro.m@gmail.com>
2021-10-25 20:13:42 -0300

    inicializa o projeto

---
+++ sisfin/src/main/java/com/sistema/financeiro/sisfin/SisfinApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.financeiro.sisfin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisfinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisfinApplication.class, args);
+	}
+
+}
---
+++ sisfin/src/test/java/com/sistema/financeiro/sisfin/SisfinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.financeiro.sisfin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisfinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
